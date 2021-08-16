e375ec0716525a91aaeabbfafea7dbb4c3914eb3
chloesheats <csheats@student.unimelb.edu.au>
2021-08-16 17:51:40 +1000

    Initial frontend react setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+/* 
+ * To run the app: 'yarn start' in terminal 
+*/
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey everyone!</h1>
+      <p>This is the beginning of our CRM.</p>
+    </div>
+  );
+}
+
+export default App;
