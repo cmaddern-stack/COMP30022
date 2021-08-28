eab8d39cfdba7f31453f1aa80f8eb803e0e4a7f8
chloesheats <chloe@sheats.org>
2021-08-28 18:30:08 +1000

    adding comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,12 @@ import logo from './logo.svg';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
-// import Nav from './components/Nav';
-// import Home from './pages/Home'
 
+/*
+To run the app on your local browser - from your terminal run:
+1. npm install 
+2. npm run start
+*/
 
 function App() {
   return (
