bbba743a170cb7fa0a798e58acf9bcbb9845a110
cmaddern-dev <86546605+cmaddern-dev@users.noreply.github.com>
2021-08-30 14:33:19 +1000

    Created simple user card for group view

--- frontend/src/pages/Groups.js
+++ frontend/src/pages/Groups.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import './Groups.css';
 
 export default function Groups() {
     
 
     return (
         <div>
-            <h1 class="text-center">This is your Groups page!</h1>
+            <div class="padded title">
+                IT Project
+            </div>
+            <div class="topContainer">
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+
+            </div>
+            <div class="topContainer">
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+
+            </div>
+            <div class="topContainer">
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+
+            </div>
+        </div>
+    )
+}
+
+function ContactCard(props) {
+    return (
+        <div class="rcorners">
+            <div class="topContainer top">
+                <div class="dot">
+                    <div class="centeredInDot">
+                        LS
+                    </div>
+                </div>
+                <div class="padded">{props.name}</div>
+            </div>
+            <div class="subText topContainer space">
+                <div class="padded3"> Title </div>
+                <div class="padded2"> it project subject coordinator </div>
+            </div>
+
+        </div>
+    )
+}
+
+function AddCard(props) {
+    return (
+        <div class="rcorners">
+            <div class="topContainer top">
+                <div class="dot">
+                    <div class="centeredInDot">
+                        LS
+                    </div>
+                </div>
+                <div class="padded">{props.name}</div>
+            </div>
+            <div class="subText topContainer space">
+                <div class="padded3"> Title </div>
+                <div class="padded2"> it project subject coordinator </div>
+            </div>
+
         </div>
     )
 }
