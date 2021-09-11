be74bc618728389906f3ad83798c796c1d3a7189
mispelledname <joanne.ngzy.is@gmail.com>
2021-09-12 00:52:00 +1000

    build enter email page

--- frontend/src/pages/auth/EnterEmail.js
+++ frontend/src/pages/auth/EnterEmail.js
@@ -1,15 +1,34 @@
 import React from "react";
+import validator from "validator";
 
 class EnterEmail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             email: "",
+            emailError: "",
+            proceed: false,
         };
     }
 
     changeHandler = (event) => {
         this.setState({ [event.target.name]: event.target.value });
+
+        if (!validator.isEmail(this.state.email)) {
+            this.setState({
+                emailError: "Enter valid email!",
+                proceed: false,
+            });
+        } else {
+            this.setState({
+                emailError: "",
+                proceed: true,
+            });
+        }
+    };
+
+    nextHandler = (event) => {
+        this.props.history.push("/");
     };
 
     render() {
@@ -25,9 +44,16 @@ class EnterEmail extends React.Component {
                         placeholder="e.g. jane.doe@email.com"
                         onChange={this.changeHandler}
                     />
+                    <span className="input-error">{this.state.emailError}</span>
                 </form>
                 <div className="button-row">
-                    <button className="primary-button" type="button" name="next">
+                    <button
+                        className="primary-button"
+                        type="button"
+                        name="next"
+                        disabled={!this.state.proceed}
+                        onClick={this.nextHandler}
+                    >
                         NEXT
                     </button>
                 </div>
