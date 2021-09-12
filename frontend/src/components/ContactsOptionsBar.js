da0670a6b36a1658afa9b0ec034c867fdb844f43
mispelledname <joanne.ngzy.is@gmail.com>
2021-09-12 23:12:53 +1000

    add documentation

--- frontend/src/components/ContactsOptionsBar.js
+++ frontend/src/components/ContactsOptionsBar.js
@@ -3,6 +3,15 @@ import './ContactsOptionsBar.css'
 import ContactsViewButton from "./ContactsViewButton";
 import SearchBar from "./SearchBar";
 
+/*
+    Contacts Options Bar 
+    refers to the bar on top of the contacts views below the Nav bar
+    - Allows users to switch between Group view and Contacts view
+    - Allows users to sort and filter
+    - Allows users to search
+    - Allows users to Add new contact by clicking on the button
+ */
+
 class ContactsOptionsBar extends React.Component {
     render() {
         return (
