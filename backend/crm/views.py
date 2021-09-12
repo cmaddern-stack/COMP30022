bdaa026af890ef2e439199b21725650586fc15ec
chloesheats <chloe@sheats.org>
2021-09-12 17:33:08 +1000

    Connecting backend to render contacts on frontend

--- backend/crm/views.py
+++ backend/crm/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework import permissions
 from .models import CustomAnswer, CustomQuestion, Contact, Event, UserProfile, Group
-from .serialisers import ContactSerializer
+from .serialisers import ContactSerializer, GroupSerializer
 
 
 # Create your views here.
@@ -17,3 +17,7 @@ class ContactViewSet(viewsets.ModelViewSet):
     serializer_class = ContactSerializer
     permission_classes = [permissions.IsAuthenticated]
     
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
