78cdc71e216dd4ed2ed6e55b2f4a0ff1bb531205
mispelledname <joanne.ngzy.is@gmail.com>
2021-09-15 14:07:01 +1000

    dark mode toggle rotation transition

--- frontend/src/components/header/DarkModeToggle.js
+++ frontend/src/components/header/DarkModeToggle.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Header.css";
-import { FontAwesome } from "react-icons/fa";
+import { FaSun, FontAwesome } from "react-icons/fa";
 import { FaMoon } from "react-icons/fa";
 
+/**
+ * Dark mode toggle
+ * changes app between light and dark mode when pressed 
+ * TODO: change to dark mode when pressed 
+ */
 class DarkModeToggle extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             mode: "off",
+            light: "active",
+            dark: "inactive"
         };
     }
 
@@ -15,10 +22,14 @@ class DarkModeToggle extends React.Component {
         if (this.state.mode === "off") {
             this.setState({
                 mode: "on",
+                light: "inactive",
+                dark: "active"
             });
         } else {
             this.setState({
                 mode: "off",
+                light: "active",
+                dark: "inactive"
             });
         }
     };
@@ -26,7 +37,8 @@ class DarkModeToggle extends React.Component {
     render() {
         return (
             <div className={"icon " + this.state.mode} onClick={this.onClick}>
-                <FaMoon />
+                <FaMoon className={this.state.dark}/>
+                <FaSun className={this.state.light}/>
             </div>
         );
     }
