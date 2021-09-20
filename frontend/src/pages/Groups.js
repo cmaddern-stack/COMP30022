5021ba55ddaf27a842d26ede4d21b55437af5b2e
cmaddern-stack <82488271+cmaddern-stack@users.noreply.github.com>
2021-09-20 15:07:57 +1000

    Group View
    - Added collapsible view
    - Added 'add contact' button
    - Added dummy create contact popup

--- frontend/src/pages/Groups.js
+++ frontend/src/pages/Groups.js
@@ -1,28 +1,66 @@
-import React from "react";
-import "./Groups.css";
-import ContactsOptionsBar from "../components/ContactsOptionsBar";
+import React from 'react';
+import Collapsible from 'react-collapsible';
+import CustomizedDialogs from '../components/Dialog';
+import './Groups.css';
 
 export default function Groups() {
+    
+
     return (
         <div>
-            <div class="padded title">IT Project</div>
+            <div class="padded title">
+                IT Project
+            </div>
+
+            <Collapsible triggerClassName="padded" trigger="Expand" triggerOpenedClassName="padded" triggerWhenOpen="Collapse" open= {true} >
+
             <div class="topContainer">
-                <ContactCard name="Leon Sterling" />
-                <ContactCard name="Leon Sterling" />
-                <ContactCard name="Leon Sterling" />
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+
             </div>
             <div class="topContainer">
-                <ContactCard name="Leon Sterling" />
-                <ContactCard name="Leon Sterling" />
-                <ContactCard name="Leon Sterling" />
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+                <AddCard name = "Leon Sterling"/>
+
+            </div>
+            </Collapsible>
+
+            <div class="padded title">
+                Real Estate
+            </div>
+
+            <Collapsible triggerClassName="padded" trigger="Expand" triggerOpenedClassName="padded" triggerWhenOpen="Collapse" open= {true} >
+
+            <div class="topContainer">
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+
             </div>
             <div class="topContainer">
-                <ContactCard name="Leon Sterling" />
-                <ContactCard name="Leon Sterling" />
-                <ContactCard name="Leon Sterling" />
+                <ContactCard name = "Leon Sterling"/>
+                <ContactCard name = "Leon Sterling"/>
+                <AddCard name = "Leon Sterling"/>
+
             </div>
+            </Collapsible>
+            
+            
+            
+            
         </div>
-    );
+    )
+}
+
+function AddContact(props) {
+    return (
+        <div className="rcorners">
+            
+        </div>
+    )
 }
 
 function ContactCard(props) {
@@ -30,7 +68,9 @@ function ContactCard(props) {
         <div class="rcorners">
             <div class="topContainer top">
                 <div class="dot">
-                    <div class="centeredInDot">LS</div>
+                    <div class="centeredInDot">
+                        LS
+                    </div>
                 </div>
                 <div class="padded">{props.name}</div>
             </div>
@@ -38,23 +78,17 @@ function ContactCard(props) {
                 <div class="padded3"> Title </div>
                 <div class="padded2"> it project subject coordinator </div>
             </div>
+
         </div>
-    );
+    )
 }
 
 function AddCard(props) {
     return (
-        <div class="rcorners">
-            <div class="topContainer top">
-                <div class="dot">
-                    <div class="centeredInDot">LS</div>
-                </div>
-                <div class="padded">{props.name}</div>
-            </div>
-            <div class="subText topContainer space">
-                <div class="padded3"> Title </div>
-                <div class="padded2"> it project subject coordinator </div>
+        <div class="rcorners lb100">
+            <div class="center iris60">
+                <CustomizedDialogs/>
             </div>
         </div>
-    );
+    )
 }
