f6900f7c345b32052a0f8bedc452e5dbeee79ecd
mispelledname <joanne.ngzy.is@gmail.com>
2021-10-22 18:04:03 +1100

    write input field test case

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,12 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-test('renders enter email page', () => {
-  render(<App />);
-  const enterEmailLabel = screen.getByText("Email Address");
-  const enterEmailIniput = screen.getByRole('textbox');
-  const nextButton = screen.getByText('NEXT');
-  expect(enterEmailLabel).toBeInTheDocument();
-  expect(enterEmailIniput).toBeInTheDocument();
-  expect(nextButton).toBeInTheDocument();
+test("renders enter email page", () => {
+    render(<App />);
+    const enterEmailLabel = screen.getByText("Email Address");
+    const enterEmailIniput = screen.getByRole("textbox");
+    const nextButton = screen.getByText("NEXT");
+    expect(enterEmailLabel).toBeInTheDocument();
+    expect(enterEmailIniput).toBeInTheDocument();
+    expect(nextButton).toBeInTheDocument();
 });
