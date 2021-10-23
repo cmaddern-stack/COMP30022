926f1a2194319a9a41ea10c69696ed1e935ddca4
mispelledname <joanne.ngzy.is@gmail.com>
2021-10-23 18:22:29 +1100

    write tests for contact card star component

--- src/components/groups/ContactCardStar.js
+++ src/components/groups/ContactCardStar.js
@@ -16,15 +16,25 @@ export default class ContactCardStar extends React.Component {
 
     render() {
         var ICON_SIZE = 23;
-        if (this.props.size !== null ) {
-            ICON_SIZE = this.props.size
+        if (this.props.size !== null) {
+            ICON_SIZE = this.props.size;
         }
         return (
-            <div onClick={this.toggleStar}>
+            <div onClick={this.toggleStar} data-testid="contact-card-star">
                 {this.props.starred ? (
-                    <div className="contact-star starred"><Star style={{ fontSize: ICON_SIZE }} /></div>
+                    <div className="contact-star starred">
+                        <Star
+                            style={{ fontSize: ICON_SIZE }}
+                            data-testid="contact-card-star--starred"
+                        />
+                    </div>
                 ) : (
-                    <div className="contact-star unstarred"><StarOutline style={{ fontSize: ICON_SIZE }} /></div>
+                    <div className="contact-star unstarred">
+                        <StarOutline
+                            style={{ fontSize: ICON_SIZE }}
+                            data-testid="contact-card-star--unstarred"
+                        />
+                    </div>
                 )}
             </div>
         );
