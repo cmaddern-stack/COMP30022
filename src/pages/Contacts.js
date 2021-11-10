3fac95cc8f54a71285916b31ce0c73f9cbbb6363
mispelledname <joanne.ngzy.is@gmail.com>
2021-11-10 17:53:57 +1100

    adjust widths on contacts page

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -164,19 +164,19 @@ export default function Contacts(props) {
         return (
             <div className="person contacts-header d-flex justify-content-between">
                 <div className="w-5"></div>
-                <div className="w-2">
+                <div className="w-5">
                     <IconContext.Provider value={{ color: "a4a6f6" }}>
                         <AiFillStar />
                     </IconContext.Provider>
                 </div>
-                <div className="table-header w-10">Name</div>
+                <div className="table-header w-name">Name</div>
                 {/* <div className="w-10">Groups</div> */}
-                {organisation ? <div className="w-10">Organisation</div> : null}
+                {organisation ? <div className="w-15">Organisation</div> : null}
                 {role ? <div className="w-10">Role</div> : null}
-                {email ? <div className="w-10">Email</div> : null}
-                {phone ? <div className="w-10">Phone</div> : null}
-                {notes ? <div className="w-15">Notes</div> : null}
-                <div className="w-5 text-right">Edit</div>
+                {email ? <div className="w-15">Email</div> : null}
+                {phone ? <div className="w-phone">Phone</div> : null}
+                {/* {notes ? <div className="w-15">Notes</div> : null} */}
+                <div className="w-edit text-right">Edit</div>
             </div>
         );
     }
@@ -214,10 +214,10 @@ export default function Contacts(props) {
                             x
                         }
                     >
-                        <div className="w-2">{showInitials(item)}</div>
-                        <div className="w-2">
+                        <div className="w-5">{showInitials(item)}</div>
+                        <div className="w-5">
                             {item.starred ? (
-                                <div className="w-2">
+                                <div className="w-5">
                                     <IconContext.Provider
                                         value={{ color: "#df5571" }}
                                     >
@@ -225,7 +225,7 @@ export default function Contacts(props) {
                                     </IconContext.Provider>
                                 </div>
                             ) : (
-                                <div className="w-2">
+                                <div className="w-5">
                                     <IconContext.Provider
                                         value={{ color: "a4a6f6" }}
                                     >
@@ -234,16 +234,15 @@ export default function Contacts(props) {
                                 </div>
                             )}
                         </div>
-                        <div className="w-10">
+                        <div className="w-name">
                             {item.firstName} {item.lastName}
                         </div>
-                        {/* <div className="w-10">Groups</div> */}
                         {organisation ? (
-                            <div className="w-10">{item.organisation}</div>
+                            <div className="w-15">{item.organisation}</div>
                         ) : null}
                         {role ? <div className="w-10">{item.role}</div> : null}
                         {email ? (
-                            <div className="w-10">
+                            <div className="w-15">
                                 <a href={"mailto:" + item.emailAddress}>
                                     {item.emailAddress}
                                 </a>
@@ -252,12 +251,12 @@ export default function Contacts(props) {
                             <div></div>
                         )}
                         {phone ? (
-                            <div className="w-10">{item.phoneNumber}</div>
+                            <div className="w-phone">{item.phoneNumber}</div>
                         ) : null}
-                        {notes ? (
+                        {/* {notes ? (
                             <div className="w-15">{item.notes}</div>
-                        ) : null}
-                        <div className="w-5 text-right">Edit</div>
+                        ) : null} */}
+                        <div className="w-edit text-right">Edit</div>
                     </div>
                 );
             });
