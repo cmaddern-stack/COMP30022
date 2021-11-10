a8619085623a6630eb69d26c8d88fb7c69559307
mispelledname <joanne.ngzy.is@gmail.com>
2021-11-11 05:42:09 +1100

    change the star icon on groups page

--- src/components/groups/ContactCardStar.js
+++ src/components/groups/ContactCardStar.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Star, StarOutline } from "@material-ui/icons";
 import "../../css/Groups.css";
 import ContactsAPI from "../../apis/contactsApi";
+import { AiOutlineStar, AiFillStar } from "react-icons/ai";
+import { IconContext } from "react-icons";
 
 export default class ContactCardStar extends React.Component {
     constructor(props) {
@@ -15,7 +17,7 @@ export default class ContactCardStar extends React.Component {
     };
 
     render() {
-        var ICON_SIZE = 23;
+        var ICON_SIZE = "16px";
         if (this.props.size !== null) {
             ICON_SIZE = this.props.size;
         }
@@ -23,17 +25,19 @@ export default class ContactCardStar extends React.Component {
             <div onClick={this.toggleStar} data-testid="contact-card-star">
                 {this.props.starred ? (
                     <div className="contact-star starred">
-                        <Star
-                            style={{ fontSize: ICON_SIZE }}
-                            data-testid="contact-card-star--starred"
-                        />
+                        <IconContext.Provider
+                            value={{ color: "#df5571", size: ICON_SIZE }}
+                        >
+                            <AiFillStar />
+                        </IconContext.Provider>
                     </div>
                 ) : (
                     <div className="contact-star unstarred">
-                        <StarOutline
-                            style={{ fontSize: ICON_SIZE }}
-                            data-testid="contact-card-star--unstarred"
-                        />
+                        <IconContext.Provider
+                            value={{ color: "a4a6f6", size: ICON_SIZE }}
+                        >
+                            <AiOutlineStar />
+                        </IconContext.Provider>
                     </div>
                 )}
             </div>
