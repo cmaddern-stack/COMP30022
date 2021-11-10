6f3f7d22dfb7a8226b793294e26fb7c776bed9ca
mispelledname <joanne.ngzy.is@gmail.com>
2021-11-10 17:20:41 +1100

    add profile icon to contacts page

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -16,14 +16,13 @@ const BASE_URL = "https://team-69-backend.herokuapp.com/crm/";
 var sortUp = false;
 
 export default function Contacts(props) {
-
     const [organisation, setOrganisation] = useState(true);
     const [role, setRole] = useState(true);
     const [email, setEmail] = useState(true);
     const [phone, setPhone] = useState(true);
     const [notes, setNotes] = useState(true);
     const [dropdown, setDropdown] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('')
+    const [searchTerm, setSearchTerm] = useState("");
     const location = useLocation();
     const history = useHistory();
 
@@ -57,34 +56,100 @@ export default function Contacts(props) {
             setContacts(data);
         }
     }, []);
-        
+
     function showDropdown() {
         if (dropdown) {
             return (
                 <div className="d-flex">
-                    <div className="searchbar"><input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/></div>
-                        
-                    <div className="dropdown-box">
-                        <button className="dropbtn" onClick={() => setDropdown(false)}><BsListUl /> Change Columns <IoMdArrowDropdown /></button>
-                        <div className="dropdown-content">
-                            { organisation ? <button onClick={() => setOrganisation(false)}>&#x2611; Show Organisation </button> : <button onClick={() => setOrganisation(true)}>&#x2610; Show Organisation </button>}
-                            { role ? <button onClick={() => setRole(false)}>&#x2611; Show Role</button> : <button onClick={() => setRole(true)}>&#x2610; Show Role</button>}
-                            { email ? <button onClick={() => setEmail(false)}>&#x2611; Show Email</button> : <button onClick={() => setEmail(true)}>&#x2610; Show Email</button>}
-                            { phone ? <button onClick={() => setPhone(false)}>&#x2611; Show Phone</button> : <button onClick={() => setPhone(true)}>&#x2610; Show Phone</button>}
-                            { notes ? <button onClick={() => setNotes(false)}>&#x2611; Show Notes</button> : <button onClick={() => setNotes(true)}>&#x2610; Show Notes</button>}
-                        </div>
-                    
+                    <div className="searchbar">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            onChange={(event) => {
+                                setSearchTerm(event.target.value);
+                            }}
+                        />
                     </div>
-                    
+
+                    <div className="dropdown-box">
+                        <button
+                            className="dropbtn"
+                            onClick={() => setDropdown(false)}
+                        >
+                            <BsListUl /> Change Columns <IoMdArrowDropdown />
+                        </button>
+                        <div className="dropdown-content">
+                            {organisation ? (
+                                <button onClick={() => setOrganisation(false)}>
+                                    &#x2611; Show Organisation{" "}
+                                </button>
+                            ) : (
+                                <button onClick={() => setOrganisation(true)}>
+                                    &#x2610; Show Organisation{" "}
+                                </button>
+                            )}
+                            {role ? (
+                                <button onClick={() => setRole(false)}>
+                                    &#x2611; Show Role
+                                </button>
+                            ) : (
+                                <button onClick={() => setRole(true)}>
+                                    &#x2610; Show Role
+                                </button>
+                            )}
+                            {email ? (
+                                <button onClick={() => setEmail(false)}>
+                                    &#x2611; Show Email
+                                </button>
+                            ) : (
+                                <button onClick={() => setEmail(true)}>
+                                    &#x2610; Show Email
+                                </button>
+                            )}
+                            {phone ? (
+                                <button onClick={() => setPhone(false)}>
+                                    &#x2611; Show Phone
+                                </button>
+                            ) : (
+                                <button onClick={() => setPhone(true)}>
+                                    &#x2610; Show Phone
+                                </button>
+                            )}
+                            {notes ? (
+                                <button onClick={() => setNotes(false)}>
+                                    &#x2611; Show Notes
+                                </button>
+                            ) : (
+                                <button onClick={() => setNotes(true)}>
+                                    &#x2610; Show Notes
+                                </button>
+                            )}
+                        </div>
+                    </div>
                 </div>
-            )
+            );
         } else {
             return (
                 <div className="d-flex">
-                    <div className="searchbar"><input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/></div>
-                    <div className="dropdown-box"><button className="dropbtn" onClick={() => setDropdown(true)}><BsListUl /> Change Columns <IoMdArrowDropleft /></button></div>
+                    <div className="searchbar">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            onChange={(event) => {
+                                setSearchTerm(event.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="dropdown-box">
+                        <button
+                            className="dropbtn"
+                            onClick={() => setDropdown(true)}
+                        >
+                            <BsListUl /> Change Columns <IoMdArrowDropleft />
+                        </button>
+                    </div>
                 </div>
-            )
+            );
         }
     }
 
@@ -99,20 +164,35 @@ export default function Contacts(props) {
         return (
             <div className="person contacts-header d-flex justify-content-between">
                 <div className="w-5"></div>
-                <div className="w-2"><IconContext.Provider value={{ color: 'a4a6f6' }}><AiFillStar /></IconContext.Provider></div>
+                <div className="w-2">
+                    <IconContext.Provider value={{ color: "a4a6f6" }}>
+                        <AiFillStar />
+                    </IconContext.Provider>
+                </div>
                 <div className="table-header w-10">Name</div>
                 {/* <div className="w-10">Groups</div> */}
-                { organisation ? <div className="w-10">Organisation</div> : null}
-                { role ? <div className="w-10">Role</div> : null}
-                { email ? <div className="w-10">Email</div> : null}
-                { phone ? <div className="w-10">Phone</div> : null}
-                { notes ? <div className="w-15">Notes</div> : null}
+                {organisation ? <div className="w-10">Organisation</div> : null}
+                {role ? <div className="w-10">Role</div> : null}
+                {email ? <div className="w-10">Email</div> : null}
+                {phone ? <div className="w-10">Phone</div> : null}
+                {notes ? <div className="w-15">Notes</div> : null}
                 <div className="w-5 text-right">Edit</div>
             </div>
         );
     }
 
     function showInitials(item) {
+        if (item.image !== null) {
+            return (
+                <div>
+                    <img
+                        alt="contact profile icon"
+                        src={item.image}
+                        id="profile-icon"
+                    ></img>
+                </div>
+            );
+        }
         const first = item.firstName[0];
         const second = item.lastName[0];
         return (
@@ -125,23 +205,61 @@ export default function Contacts(props) {
 
     function renderItems() {
         if (contacts.length > 0) {
-
             return contacts.map((item, index) => {
                 let x = index % 2;
                 return (
-                    <div className={'person d-flex white justify-content-between color-' + x}>
+                    <div
+                        className={
+                            "person d-flex white justify-content-between color-" +
+                            x
+                        }
+                    >
                         <div className="w-2">{showInitials(item)}</div>
-                        <div className="w-2">{ item.starred ? <div className="w-2"><IconContext.Provider value={{ color: '#df5571' }}><AiFillStar /></IconContext.Provider></div> : <div className="w-2"><IconContext.Provider value={{ color: 'a4a6f6' }}><AiOutlineStar /></IconContext.Provider></div>}</div>
-                        <div className="w-10">{item.firstName} {item.lastName}</div>
+                        <div className="w-2">
+                            {item.starred ? (
+                                <div className="w-2">
+                                    <IconContext.Provider
+                                        value={{ color: "#df5571" }}
+                                    >
+                                        <AiFillStar />
+                                    </IconContext.Provider>
+                                </div>
+                            ) : (
+                                <div className="w-2">
+                                    <IconContext.Provider
+                                        value={{ color: "a4a6f6" }}
+                                    >
+                                        <AiOutlineStar />
+                                    </IconContext.Provider>
+                                </div>
+                            )}
+                        </div>
+                        <div className="w-10">
+                            {item.firstName} {item.lastName}
+                        </div>
                         {/* <div className="w-10">Groups</div> */}
-                        { organisation ? <div className="w-10">{item.organisation}</div> : null}
-                        { role ? <div className="w-10">{item.role}</div> : null}
-                        { email ? <div className="w-10"><a href={'mailto:' + item.emailAddress} >{item.emailAddress}</a></div> : <div></div>}
-                        { phone ? <div className="w-10">{item.phoneNumber}</div> : null}
-                        { notes ? <div className="w-15">{item.notes}</div> : null}
+                        {organisation ? (
+                            <div className="w-10">{item.organisation}</div>
+                        ) : null}
+                        {role ? <div className="w-10">{item.role}</div> : null}
+                        {email ? (
+                            <div className="w-10">
+                                <a href={"mailto:" + item.emailAddress}>
+                                    {item.emailAddress}
+                                </a>
+                            </div>
+                        ) : (
+                            <div></div>
+                        )}
+                        {phone ? (
+                            <div className="w-10">{item.phoneNumber}</div>
+                        ) : null}
+                        {notes ? (
+                            <div className="w-15">{item.notes}</div>
+                        ) : null}
                         <div className="w-5 text-right">Edit</div>
                     </div>
-                )
+                );
             });
 
             // return contacts.filter((val) => {
@@ -195,4 +313,3 @@ export default function Contacts(props) {
         </div>
     );
 }
-
